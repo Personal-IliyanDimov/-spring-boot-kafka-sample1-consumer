21f8e930be23b024e6f63e84cc628859012eb21c
Personal-IliyanDimov <ilianmd@abv.bg>
2020-11-27 02:22:13 +0200

    Integration between producer and consumer (working)

--- src/main/java/org/imd/kafka/sample1/consumer/config/CloudStreamConfig.java
+++ src/main/java/org/imd/kafka/sample1/consumer/config/CloudStreamConfig.java
@@ -8,7 +8,6 @@ import org.imd.kafka.sample1.consumer.service.exception.AuctionAlreadyExistsExce
 import org.imd.kafka.sample1.consumer.service.exception.AuctionNotExistException;
 import org.imd.kafka.sample1.consumer.service.exception.AuctionNotStartedException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.function.context.config.RoutingFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
@@ -23,11 +22,6 @@ public class CloudStreamConfig {
 
     @Autowired
     private ArbiterService arbiterService;
-
-    @Bean
-    public RoutingFunction routingFunction() {
-        return null;
-    }
 
     @Bean
     public Consumer<Flux<AuctionEvent>> auctionEventConsumer() {
@@ -54,19 +48,4 @@ public class CloudStreamConfig {
                  }
             });
     }
-
-
-//    @Bean
-//    public static Function<Flux<AuctionBidEvent>, Tuple2<Flux<AuctionBidEvent>, Flux<AuctionBidEvent>>> scatter() {
-//        return flux -> {
-//            Flux<AuctionBidEvent> connectedFlux = flux.publish().autoConnect(2);
-//            UnicastProcessor htlUnicastProcessor = UnicastProcessor.create();
-//            UnicastProcessor bpUnicastProcessor = UnicastProcessor.create();
-//            Flux<AuctionBidEvent> evenFlux = connectedFlux.filter(bidEvent -> number % 2 == 0).doOnNext(bidEvent -> htlUnicastProcessor.onNext(bitEvent));
-//            Flux<AuctionBidEvent> oddFlux = connectedFlux.filter(bidEvent -> number % 2 != 0).doOnNext(number -> odd.onNext("ODD: " + number));
-//
-//            return Tuples.of(Flux.from(even).doOnSubscribe(x -> evenFlux.subscribe()), Flux.from(odd).doOnSubscribe(x -> oddFlux.subscribe()));
-//        };
-//    }
-
 }
---
+++ src/main/java/org/imd/kafka/sample1/consumer/config/ObjectMapperConfig.java
@@ -0,0 +1,29 @@
+package org.imd.kafka.sample1.consumer.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    //
+    // ugly hack in order streams to start working.
+    // used especially to override the same in
+    // ApplicationJsonMessageMarshallingConverter.
+    // used also to fix the shit in
+    // method MappingJackson2MessageConverter.convertFromInternal
+    // where json views are used during parsing
+    //
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.findAndRegisterModules();
+        return objectMapper;
+    }
+}
